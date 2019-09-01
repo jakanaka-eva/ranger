jakanaka-eva/ranger

commit 5b0b73fc182a582361dff754d3ecafa8a16e3292

fixes setlocal on dirs with regex metachars

ranger/container/settings.py
@@ -276,7 +276,7 @@ class Settings(SignalDispatcher, FileManagerAware):
         if path:
             if path not in self._localsettings:
                 try:
-                    regex = re.compile(path)
+                    regex = re.compile(re.compile(path))
                 except re.error:  # Bad regular expression
                     return
                 self._localregexes[path] = regex

commit 49dfbf9c8cd3bfed7b89c635352841be5ff98112

Make setlocal path=... argument a regex again
The man page says that the path argument to the setlocal command is a
regex. This commit fixes the regression in 5b0b73fc, which made it a
normal string.

ranger/config/commands.py
@@ -502,7 +502,7 @@ class setlocal(set_):
         if path is None:
             path = self._re_shift(self.PATH_RE_UNQUOTED.match(self.arg(1)))
         if path is None and self.fm.thisdir:
-            path = self.fm.thisdir.path
+            path = "^" + re.escape(self.fm.thisdir.path) + "$"
         if not path:
             return
 

ranger/container/settings.py
@@ -278,7 +278,7 @@ class Settings(SignalDispatcher, FileManagerAware):
         if path:
             if path not in self._localsettings:
                 try:
-                    regex = re.compile(re.escape(path))
+                    regex = re.compile(path)
                 except re.error:  # Bad regular expression
                     return
                 self._localregexes[path] = regex